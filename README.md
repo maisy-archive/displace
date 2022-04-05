6e7da114c89da8d71f7a1d204ba85b60b34f7d93
Beef <beefers@riseup.net>
2022-04-05 07:43:12 +0100

    [Bugfix] Despite place having ended - fix canvasIndexes

--- README.md
+++ README.md
@@ -4,16 +4,6 @@
 
 ### Usage
 
-If given no arguments, the program will scrape the entire 2000x2000 board. This takes a lot of time and bandwidth.
-
-```sh
-node . <starting x coordinate, 0 - 999> <starting y coordinate 0 - 999> <starting canvasIndex, 0 - 3> <ending x coordinate, 0 - 999> <ending y coordinate 0 - 999> <ending canvasIndex, 0 - 3>
+```js
+node .
 ```
-
-#### Example
-
-```sh
-node . 100 100 0 100 100 1
-```
-
-This would scrape all usernames between 100,100 on canvas 1 and 100,100 on canvas 2.
--- src/index.js
+++ src/index.js
@@ -1,39 +1,11 @@
 import getPixel from "./lib/getPixel.js";
 
-const args = process.argv.slice(2);
-
-let start = {
-    x: args[0] || 0,
-    y: args[1] || 0,
-    canvasIndex: args[2] || 0
-}
-
-let end = {
-    x: args[3] || 999,
-    y: args[4] || 999,
-    canvasIndex: args[5] || 3
-}
-
-console.log(start)
-console.log(end)
-
-let currentX = start.x;
-let currentY = start.y;
-let currentCanvasIndex = start.canvasIndex;
-
-while(currentCanvasIndex < end.canvasIndex + 1) {
-    if (currentCanvasIndex > end.canvasIndex) process.exit();
-
-    if (currentX > end.x + 1 && currentY > end.y + 1) {
-        currentX = start.x;
-        currentY = start.y;
-        currentCanvasIndex++;
-    } else if(currentX > end.x) {
-        currentX = start.x;
-        currentY++;
-    } else {
-        const pixelInfo = await getPixel(currentX, currentY, currentCanvasIndex);
-        console.log(`coordinate: ${currentX},${currentY} | canvasIndex: ${currentCanvasIndex} | ${new Date(pixelInfo.lastModifiedTimestamp)} | ${pixelInfo.userInfo.username} (${pixelInfo.userInfo.userID})`);
-        currentX++;
+const size = 1000;
+for (let q = 0; q < 4; q++) {
+  for (let x = 0; x < size; x++) {
+    for (let y = 0; y < size; y++) {
+        const pixelInfo = await getPixel(x, y, q);
+        console.log(`coordinate: ${x},${y} | canvasIndex: ${q} | ${new Date(pixelInfo.lastModifiedTimestamp)} | ${pixelInfo.userInfo.username} (${pixelInfo.userInfo.userID})`);
     }
+  }
 }
