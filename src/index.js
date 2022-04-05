079e953082696dd559b70eb233de14cca50e5ee5
Beef <beefers@riseup.net>
2022-04-05 16:55:59 +0100

    [Bugfix] Invalid user handling (really kinda needed now)

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ for (let q = 0; q < 4; q++) {
   for (let x = 0; x < size; x++) {
     for (let y = 0; y < size; y++) {
         const pixelInfo = await getPixel(x, y, q);
-        console.log(`coordinate: ${x},${y} | canvasIndex: ${q} | ${new Date(pixelInfo.lastModifiedTimestamp)} | ${pixelInfo.userInfo.username} (${pixelInfo.userInfo.userID})`);
+        console.log(`coordinate: ${x},${y} | canvasIndex: ${q} | ${new Date(pixelInfo.lastModifiedTimestamp)} | ${pixelInfo.userInfo ? pixelInfo.userInfo.username : "No user"} (${pixelInfo.userInfo ? pixelInfo.userInfo.userId : "null"})`);
     }
   }
 }
